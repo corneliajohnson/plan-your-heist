4aceaeb02f9d82ed92ab12160e847bfe34241ae0
corneliajohnson <corneliaj91@gmail.com>
2020-11-20 14:35:24 -0500

    enter trail runs

--- Program.cs
+++ Program.cs
@@ -20,52 +20,54 @@ namespace PlanYourHeist
 
       //Print the message "Plan Your Heist!".
       Console.WriteLine("Plan Your Heist!");
-
-      //Collect several team members' information.
-      Console.Write("Add A New Member? Y/N  ");
-      string answer = Console.ReadLine().ToLower();
       Console.WriteLine();
-      while (answer == "y")
+      Console.Write("Enter the number of trial runs the program should perform:  ");
+      int trailRuns = int.Parse(Console.ReadLine());
+      int count = 0;
+      while (count < trailRuns)
       {
-        newTeamMember = CreateTeamMember();
-        //Stop collecting team members when a blank name is entered.
-        if (!string.IsNullOrEmpty(newTeamMember.Name))
+        //Collect several team members' information.
+        Console.WriteLine();
+        Console.Write("Add A New Member? Y/N  ");
+        string answer = Console.ReadLine().ToLower();
+        Console.WriteLine();
+        while (answer == "y")
         {
-          wholeTeam.Add(newTeamMember);
-          Console.Write("Add A New Member? Y/N  ");
-          answer = Console.ReadLine().ToLower();
-          Console.WriteLine();
+          newTeamMember = CreateTeamMember();
+          //Stop collecting team members when a blank name is entered.
+          if (!string.IsNullOrEmpty(newTeamMember.Name))
+          {
+            wholeTeam.Add(newTeamMember);
+            Console.WriteLine();
+            Console.Write("Add A New Member? Y/N  ");
+            answer = Console.ReadLine().ToLower();
+            Console.WriteLine();
+          }
+          else
+          {
+            Console.WriteLine(@"Error Member Must have name try again
+          NAMELESS MEMBER WAS NOT ADDED");
+          }
+        }
+
+        //Sum the skill levels of the team. Save that number.
+        IEnumerable<int> total = wholeTeam.Select(member => member.SkillLevel);
+        int teamTotalSkillLevel = total.Sum();
+
+        //Before displaying the success or failure message, display  team's combined skill level and bank's difficulty level
+        Console.WriteLine($"Team Cobined Skill Level: {teamTotalSkillLevel}");
+        Console.WriteLine($"Bank Difficulty Level: { bankDifficultyLevel + luckValue}");
+
+        //Compare the number with the bank's difficulty level. If the team's skill level is greater than or equal to the bank's difficulty level, Display a success message, otherwise display a failure message.
+        if (teamTotalSkillLevel >= bankDifficultyLevel + luckValue)
+        {
+          Console.WriteLine("Money! Money! Money! Monday!");
         }
         else
         {
-          Console.WriteLine(@"Error Member Must have name try again
-          NAMELESS MEMBER WAS NOT ADDED");
+          Console.WriteLine("Being Free anb broke is better than jail and broke.");
         }
-      }
-      //Display a message containing the number of members of the team.
-      // Console.WriteLine($"You Have {wholeTeam.Count} Team Members");
-      // wholeTeam.ForEach(member => Console.WriteLine($@"
-      //   Name: {member.Name}
-      //   Skill Level: {member.SkillLevel}
-      //   Courage: {member.Courage}
-      //   "));
-
-      //Sum the skill levels of the team. Save that number.
-      IEnumerable<int> total = wholeTeam.Select(member => member.SkillLevel);
-      int teamTotalSkillLevel = total.Sum();
-
-      //Before displaying the success or failure message, display  team's combined skill level and bank's difficulty level
-      Console.WriteLine($"Team Cobined Skill Level: {teamTotalSkillLevel}");
-      Console.WriteLine($"Bank Difficulty Level: {teamTotalSkillLevel + luckValue}");
-
-      //Compare the number with the bank's difficulty level. If the team's skill level is greater than or equal to the bank's difficulty level, Display a success message, otherwise display a failure message.
-      if (teamTotalSkillLevel + luckValue >= bankDifficultyLevel)
-      {
-        Console.WriteLine("Money! Money! Money! Monday!");
-      }
-      else
-      {
-        Console.WriteLine("Being Free anb broke is better than jail and broke.");
+        count++;
       }
     }
 
