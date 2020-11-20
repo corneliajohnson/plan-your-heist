3639b5d43cce61f38faee89e97dc6a96ef728fb1
corneliajohnson <corneliaj91@gmail.com>
2020-11-20 13:23:54 -0500

    Sum the skill levels of the team. Save that number

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlanYourHeist
 {
@@ -11,12 +12,16 @@ namespace PlanYourHeist
       var wholeTeam = new List<TeamMember>();
       var newTeamMember = new TeamMember();
 
+      //Store a value for the bank's difficulty level. Set this value to 100.
+      int bankDifficultyLevel = 100;
+
       //Print the message "Plan Your Heist!".
       Console.WriteLine("Plan Your Heist!");
 
       //Collect several team members' information.
       Console.Write("Add A New Member? Y/N  ");
       string answer = Console.ReadLine().ToLower();
+      Console.WriteLine();
       while (answer == "y")
       {
         newTeamMember = CreateTeamMember();
@@ -26,6 +31,7 @@ namespace PlanYourHeist
           wholeTeam.Add(newTeamMember);
           Console.Write("Add A New Member? Y/N  ");
           answer = Console.ReadLine().ToLower();
+          Console.WriteLine();
         }
         else
         {
@@ -34,12 +40,16 @@ namespace PlanYourHeist
         }
       }
       //Display a message containing the number of members of the team.
-      Console.WriteLine($"You Have {wholeTeam.Count} Team Members");
-      wholeTeam.ForEach(member => Console.WriteLine($@"
-        Name: {member.Name}
-        Skill Level: {member.SkillLevel}
-        Courage: {member.Courage}
-        "));
+      // Console.WriteLine($"You Have {wholeTeam.Count} Team Members");
+      // wholeTeam.ForEach(member => Console.WriteLine($@"
+      //   Name: {member.Name}
+      //   Skill Level: {member.SkillLevel}
+      //   Courage: {member.Courage}
+      //   "));
+
+      //Sum the skill levels of the team. Save that number.
+      IEnumerable<int> total = wholeTeam.Select(member => member.SkillLevel);
+      int teamTotalSkillLevel = total.Sum();
     }
 
     static TeamMember CreateTeamMember()
